49f647e60422854ca73dbe39c340d447d4a8a3be
npatskan <npatskan@vt.edu>
2020-04-03 17:46:10 -0400

    echo works, need FSM

--- homework4.h
+++ homework4.h
@@ -1,8 +1,8 @@
 /*
  * homework4.h
  *
- *  Created on:
- *      Author:
+ *  Created on: 4/2/2020
+ *      Author: Nathan Patskan
  */
 
 #ifndef HOMEWORK4_H_
@@ -13,6 +13,21 @@ void initBoard();
 bool charFSM(char rChar);
 
 // TODO: Define any constants that are local to homework.c using #define
+
+// This function returns the received char or 0xFF if no char is available
+// valid args are
+//   - \b EUSCI_A0_BASE
+//   - \b EUSCI_A1_BASE
+//   - \b EUSCI_A2_BASE
+//   - \b EUSCI_A3_BASE
+
+uint8_t UARTGetChar(uint32_t moduleInstance);
+
+
+// This function Echos the received char
+
+void UART_Echo(uint32_t moduleInstance, char transmitChar);
+
 
 
 #endif /* HOMEWORK4_H_ */
